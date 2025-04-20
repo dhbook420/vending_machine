dae66a405b0c5006421fccb7c46a2a66d7c3d624
book3pro <dhbook420@gmail.com>
2025-04-20 19:44:51 +0900

    widget

---
+++ widget.cpp
@@ -0,0 +1,96 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+    , money(0)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::changeMoney(int diff)
+{
+    money += diff;
+    ui->lcdNumber->display(money);
+}
+
+void Widget::on_pb10_clicked()
+{
+    changeMoney(10);
+}
+
+
+void Widget::on_pb50_clicked()
+{
+    changeMoney(50);
+}
+
+
+void Widget::on_pb100_clicked()
+{
+    changeMoney(100);
+}
+
+
+void Widget::on_pb500_clicked()
+{
+    changeMoney(500);
+}
+
+
+void Widget::on_pbTea_clicked()
+{
+    if (money >= 150)
+        changeMoney(-150);
+}
+
+
+void Widget::on_pbCoffee_clicked()
+{
+    if (money >= 100)
+        changeMoney(-100);
+}
+
+
+void Widget::on_pbCoke_clicked()
+{
+    if (money >= 200)
+        changeMoney(-200);
+}
+
+
+void Widget::on_pbReset_clicked()
+{
+    QMessageBox mb;
+    if (money > 0)
+    {
+        int remaining = money;
+
+        int n_500 = remaining / 500;
+        remaining %= 500;
+
+        int n_100 = remaining / 100;
+        remaining %= 100;
+
+        int n_50 = remaining / 50;
+        remaining %= 50;
+
+        int n_10 = remaining / 10;
+        remaining %= 10;
+
+        QString text = QString("거스름돈\n500원: %1개\n100원: %2개\n50원: %3개\n10원: %4개").arg(n_500).arg(n_100).arg(n_50).arg(n_10);
+
+        mb.information(this, "Reset", text);
+        changeMoney(-money);
+    }
+}
+
---
+++ widget.h
@@ -0,0 +1,46 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Widget;
+}
+QT_END_NAMESPACE
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+    int money;
+
+public:
+    Widget(QWidget *parent = nullptr);
+    ~Widget();
+    void changeMoney(int diff);
+
+
+
+private slots:
+    void on_pb10_clicked();
+
+    void on_pb50_clicked();
+
+    void on_pb100_clicked();
+
+    void on_pb500_clicked();
+
+    void on_pbTea_clicked();
+
+    void on_pbCoffee_clicked();
+
+    void on_pbCoke_clicked();
+
+    void on_pbReset_clicked();
+
+private:
+    Ui::Widget *ui;
+};
+#endif // WIDGET_H
+
+
